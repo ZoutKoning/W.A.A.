fa6524f788d3b83062ef340b590248fdd5587821
ZoutKoning <jfscyber9@gmail.com>
2022-12-05 10:23:01 -0500

    added random return to activities.

--- CPSC 3720/ActivityList.py
+++ CPSC 3720/ActivityList.py
@@ -13,23 +13,29 @@ def Activites(weather):
     if(weather in hot_weather):
         #HotWeather act  (swim, pool, lake, beach)...
         hot_weather_acts = ["Go for a Swim", "Locate (nearest) Icecream", "Stand infront of A/C"]
-        pass
+        
+        return random.choice(hot_weather_acts)
+    
     if(weather in good_weather):
         #good weather (go for a walk, run, park, picnic, bike, rip some kickball)...
         good_weather_acts = ["Go for a Walk", "Go for a Run", "Go to the Park", "Go on a Picnic", "Go for a Bike Ride", "Play some Outdoor Sports"]
-        pass
+        
+        return random.choice(good_weather_acts)
+    
     if(weather in rainy_weather):
         #rainy weather acts (go to gym, go see a movie, go shopping, do a puzzle, read a book, video games)...
         good_rainy_acts = ["Go to the Gym", "Go See a Movie", "Go Shopping", "Do a Puzzle", "Read a Book", "Play a Video Game"]
-        pass
+        
+        #Replace pass with the return value
+        return random.choice(good_rainy_acts)
+    
     if(weather in snowy_weather):
-        good_snowy_acts = ["Go Sledding", "Have Hot-Coco", "Build a Snowman", "Have a Snowball Fight", "Go Skiing","Go Snowboarding"]
         #snowy weather acts (sledding, build a snowman, ski, sled)(Building Snow man Snowball fights Snowboard)...
-        pass
+        good_snowy_acts = ["Go Sledding", "Have Hot-Coco", "Build a Snowman", "Have a Snowball Fight", "Go Skiing","Go Snowboarding"]
+        
+        return random.choice(good_snowy_acts)
+    
     if(weather in stormy_weather):
         stormy_weather_acts = ["Read a book", "Watch a Movie", "Do a Puzzle", "Play a Video Game","Bake Cookies"]
-        #stormy weather acts (go see a movie, go shopping, do a puzzle, read a book, video games)...
-        #(Reading book at home Playing board games at home Watching movies/TV shows at home Bake cookies
-
-        pass
-    
+        
+        return random.choice(stormy_weather_acts)
