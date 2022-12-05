43cab159510b7aa6e42bc136d7312dab25190911
ZoutKoning <jfscyber9@gmail.com>
2022-12-05 10:32:40 -0500

    added variable for GUI to call for activities

--- CPSC 3720/ActivityList.py
+++ CPSC 3720/ActivityList.py
@@ -7,36 +7,42 @@ def Activites(weather):
     good_weather = ["clear","fair", "clearn", "fairn", "hazy", "hazyn", "pcloudy", "pcloudyn", "sunny", "sunnyw", "wind", "windn"]
     rainy_weather = ["rain", "rainandsnow", "showers", "showersw", "wintrymix", "snowtorain","rainn", "rainandsnown", "showersn", "showerswn", "wintrymixn", "snowtorainn", "pcloudyr", "pcloudyrn", "pcloudyrw", "pcloudyrwn"],
     snowy_weather = ["blizzard","blowingsnow", "blizzardn", "blowingsnown", "sleet", "sleetn", "pcloudys", "pcloudysn"],
-    stormy_weather = ["tstorm","tstorms", "tstormsw","tstormn", "tstormsn", "tstormswn"],
-   
-    # String Compare passed in weather to lists
+    stormy_weather = ["tstorm","tstorms", "tstormsw","tstormn", "tstormsn", "tstormswn"]
     
+    # Variable to call for the GUI to get the returns 
+    ret_act
+    # String Compare passed in weather to lists
     if(weather in hot_weather):
         #HotWeather act  (swim, pool, lake, beach)...
         hot_weather_acts = ["Go for a Swim", "Locate (nearest) Icecream", "Stand infront of A/C"]
         
-        return random.choice(hot_weather_acts)
+        ret_act = random.choice(hot_weather_acts)
+        return ret_act
     
     if(weather in good_weather):
         #good weather (go for a walk, run, park, picnic, bike, rip some kickball)...
         good_weather_acts = ["Go for a Walk", "Go for a Run", "Go to the Park", "Go on a Picnic", "Go for a Bike Ride", "Play some Outdoor Sports"]
         
-        return random.choice(good_weather_acts)
+        ret_act = random.choice(good_weather_acts)
+        return ret_act
     
     if(weather in rainy_weather):
         #rainy weather acts (go to gym, go see a movie, go shopping, do a puzzle, read a book, video games)...
         good_rainy_acts = ["Go to the Gym", "Go See a Movie", "Go Shopping", "Do a Puzzle", "Read a Book", "Play a Video Game"]
         
         #Replace pass with the return value
-        return random.choice(good_rainy_acts)
+        ret_act = random.choice(good_rainy_acts)
+        return ret_act
     
     if(weather in snowy_weather):
         #snowy weather acts (sledding, build a snowman, ski, sled)(Building Snow man Snowball fights Snowboard)...
         good_snowy_acts = ["Go Sledding", "Have Hot-Coco", "Build a Snowman", "Have a Snowball Fight", "Go Skiing","Go Snowboarding"]
         
-        return random.choice(good_snowy_acts)
+        ret_act = random.choice(good_snowy_acts)
+        return ret_act
     
     if(weather in stormy_weather):
         stormy_weather_acts = ["Read a book", "Watch a Movie", "Do a Puzzle", "Play a Video Game","Bake Cookies"]
         
-        return random.choice(stormy_weather_acts)
+        ret_act = random.choice(stormy_weather_acts)
+        return ret_act
